0c83bfd841b891da6285b8b844b7833d29e3fcce
Laloka2000 <laszlocodingonly@gmail.com>
2025-01-29 21:16:08 +0100

    input 0 or 1 cases handeld

--- script.js
+++ script.js
@@ -2,20 +2,13 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
-const decimalToBinary = (input) =>{
-    let binary = "";
-    
-    if(input === 0){
-        binary = "0";
+const decimalToBinary = (input) => {
+    if (input === 0 || input === 1) {
+      return String(input);
+    } else {
+      return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
-
-    while(input > 0){
-        binary = (input % 2) + binary;
-        input = Math.floor(input / 2);
-    }
-
-    result.innerText = binary;
-}
+  };
 
 
 const checkUserInput = () => {
@@ -23,7 +16,7 @@ const checkUserInput = () => {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
 };
 
